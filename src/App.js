e682b822701f357a96478e92f0de672b940c083b
markporo <markporo@yahoo.com>
2021-06-16 21:25:51 -0700

    adding routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Route, Link, Switch, useHistory } from "react-router-dom";
 import './App.css';
 import LoginForm from './components/loginForm'
+import Home from "./components/home"
 import * as yup from 'yup';
 
 function App() {
@@ -9,10 +10,17 @@ function App() {
   const [passwordSignUp, setPasswordSignUp] = useState("");
 
 
-  return (
-    <div className="App">
+  return (<div className="App">
+    <h1>Water My Plants</h1>
+    <nav>
+      <p><Link to='/'>HOME</Link></p>
+      <p><Link id="login-form" to='/login-form'>Login/ Sign Up</Link></p>
+    </nav>
+    <switch>
+      <Home></Home>
       <LoginForm setUserNameSignUp={setUserNameSignUp} setPasswordSignUp={setPasswordSignUp}></LoginForm>
-    </div>
+    </switch>
+  </div>
   );
 }
 
